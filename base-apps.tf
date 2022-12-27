8ab36ea2552cdb2ab915ce4a682804f22ac9d759
Nate <github@nategramer.com>
2022-12-27 16:13:13 -0600

    Set ref, depenencies

--- base-apps.tf
+++ base-apps.tf
@@ -1,3 +1,7 @@
 module istio {
-  source = "git::git@github.com:n8tg/kubernetes-base-apps.git//.?ref=main"
+  depends_on = [
+    google_container_cluster.primary,
+    google_container_node_pool.primary_nodes
+  ]
+  source = "git::git@github.com:n8tg/kubernetes-base-apps.git//.?ref=0.0.1"
 }
