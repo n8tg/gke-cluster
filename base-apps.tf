34e6d804f3cf0fd12b1c39dfb6d1cb2b02c052e7
Nate <github@nategramer.com>
2022-12-28 16:21:06 -0600

    bump version

--- base-apps.tf
+++ base-apps.tf
@@ -3,5 +3,5 @@ module "istio" {
     google_container_cluster.primary,
     google_container_node_pool.primary_nodes
   ]
-  source = "git::git@github.com:n8tg/kubernetes-base-apps.git//.?ref=0.0.2"
+  source = "git::git@github.com:n8tg/kubernetes-base-apps.git//.?ref=0.0.3"
 }
