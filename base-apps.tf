03d9080e68640cfcecb97f24af1e90dd2c3d77e5
n8 <github@nategramer.com>
2022-12-29 11:30:20 -0600

    Remove base apps from cluster tf

--- base-apps.tf
+++
@@ -1,7 +0,0 @@
-module "istio" {
-  depends_on = [
-    google_container_cluster.primary,
-    google_container_node_pool.primary_nodes
-  ]
-  source = "git::git@github.com:n8tg/kubernetes-base-apps.git//.?ref=0.0.3"
-}
